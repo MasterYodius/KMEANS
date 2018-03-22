a5b027b50a2b3bee8312f5500e091c8502f99491
MasterYodius <alex-du-93500@hotmail.fr>
2018-03-22 15:01:22 +0100

    Add files via upload

--- Controleur.java
+++ Controleur.java
@@ -5,12 +5,44 @@ public class Controleur {
 	private ArrayList<Point> points;
 	private ArrayList<Integer> groupes;
 	
-	public Controleur(int nbG){
+	public Controleur(int nbG,ArrayList<Point> p){
+		this.points = new ArrayList<Point>(p);
 		int i;
-		for(i=0; i<nbG ;i++)
+		for(i=0; i<this.points.size() ;i++)
 		{
-			this.groupes.add(i);
+			points.get(i).setGroupe((int) (Math.random()*(nbG)));//Inclus ?
 		}
+		
 	}
+	
+	public ArrayList<Float> calculCentroide(int c){
+		int i=0,res=0;
+		for(Point p:points){
+			if(p.getGroupe()==c)
+				for(float f:p.getCoordonnees())
+				{
+					
+				}
+			i++;
+		}
+		return (res/i);
+	}
+	
+	
+	public void chercherCentroides(){
+		this.groupes=new ArrayList<Integer>();
+		for(int i=0;i<groupes.size();i++)
+			groupes.add(i,calculCentroide(i));
+	} 
+
+	public ArrayList<Integer> getGroupes() {
+		return groupes;
+	}
+
+	public void setGroupes(ArrayList<Integer> groupes) {
+		this.groupes = groupes;
+	}
+	
+	
 
 }
